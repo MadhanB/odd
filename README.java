3d4b21a9966e248149642ea1445d44c901fbd313
MadhanB <smartmadhan07@gmail.com>
2016-07-09 10:15:46 +0530

    finish

---
+++ README.java
@@ -0,0 +1,15 @@
+# odd
+class madhan
+{
+public static voidmain(Strings[] args)
+{
+int a;
+Sysytem.out.println("Enter a number");
+Scanner input = new Scanner(System.in);
+a=input.nextInt();
+if(a%2==0)
+{
+Sysytem.out.println("number is even");
+else
+Sysytem.out.println("Enter a odd");
+}}}
